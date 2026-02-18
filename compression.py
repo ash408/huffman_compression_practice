cd26d2d58dfb3ce8debb47b50cbb014b0b9f3bda
ash408 <124532093+ash408@users.noreply.github.com>
2026-02-17 22:48:25 -0500

    Move frequency dict to node list conversion into Tree class

--- compression.py
+++ compression.py
@@ -10,7 +10,7 @@ import base64
 def main(location="test.b64"):
 	encoded_data = retrieve_data(location)
 	frequency_dict = find_frequency(encoded_data)
-	nodes = frequency_to_nodes(frequency_dict)
+	nodes = Tree.frequency_to_nodes(frequency_dict)
 
 	for node in nodes:
 		print(node.character + ": " + str(node.frequency))
@@ -20,30 +20,6 @@ def retrieve_data(location):
 	encoded_data = base64.b64encode(b64_data).decode('utf-8')
 	
 	return encoded_data
-
-def find_frequency(data):
-	frequency_dict = {}
-
-	for character in data:
-		if character in frequency_dict:
-			num = frequency_dict[character]
-			frequency_dict[character] = num + 1
-		
-		else:
-			frequency_dict[character] = 1
-
-	return frequency_dict
-
-def frequency_to_nodes(frequency_dict):
-	nodes = []
-
-	for character in sorted(frequency_dict, key=frequency_dict.get, reverse=True):
-		frequency = frequency_dict[character]
-
-		new_node = Node(character, frequency)
-		nodes.append(new_node)
-
-	return nodes
 
 
 if __name__ == "__main__":
--- tree.py
+++ tree.py
@@ -3,6 +3,17 @@ class Tree:
 	def __init__(self, root):
 		self.root = root
 
+	def frequency_to_nodes(frequency_dict):
+		nodes = []
+
+		for character in sorted(frequency_dict, key=frequency_dict.get, reverse=True):
+			frequency = frequency_dict[character]
+
+			new_node = Node(character, frequency)
+			nodes.append(new_node)
+
+		return nodes
+
 
 class Node:
 	def __init__(self, character, frequency):
@@ -11,3 +22,5 @@ class Node:
 
 		self.left = None
 		self.right = None
+
+

9f9504772cab3961ba8012628d95a4c7103f64d9
ash408 <124532093+ash408@users.noreply.github.com>
2026-03-03 09:52:31 -0500

    Compress b64 data using huffman codes

--- compression.py
+++ compression.py
@@ -11,11 +11,22 @@ def main(location="test.b64"):
 	codes = TreeBuilder(encoded_data).get_codes()
 	file_handling.save_codes(codes)
 
+	binary_data = compress_data(encoded_data, codes)
+
 def retrieve_data(location):
 	b64_data = file_handling.read_file_b64(location)
 	encoded_data = base64.b64encode(b64_data).decode('utf-8')
 	
 	return encoded_data
+
+def compress_data(encoded_data, codes):
+	binary_data = ""
+
+	for character in encoded_data:
+		binary_data = binary_data + codes[character]
+
+	print(binary_data)
+	return int(binary_data, base=2)
 
 
 if __name__ == "__main__":
